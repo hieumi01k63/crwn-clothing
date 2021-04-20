ec6cf205f05186b7e107610fb83cdbb6945b6872
hieumi01k63 <nguyentrunghieu6934@gmail.com>
2021-04-20 13:21:59 +0700

    styled menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.style.scss";
+import { Directory } from "../../components/directory/directory.component";
+
+export const HomePage = () => (
+  <div className="homePage">
+    <Directory></Directory>
+  </div>
+);
