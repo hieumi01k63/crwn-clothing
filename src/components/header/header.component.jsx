62bd4cafbad5c9b86801889523d678096ce19035
hieumi01k63 <nguyentrunghieu6934@gmail.com>
2021-04-23 21:47:47 +0700

    Header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.style.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo"></Logo>
+      </Link>
+
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
